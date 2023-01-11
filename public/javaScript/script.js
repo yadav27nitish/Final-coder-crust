d8fe774ea47c05ed1998866d23df483bcfaf6cbf
yadav27nitish <yadav27nitish@gmail.com>
2023-01-11 21:20:47 +0530

    fix

--- public/javaScript/script.js
+++ public/javaScript/script.js
@@ -1,8 +1,7 @@
-function openNav(){
-document.getElementById("mobile__menu").style.width="100%";
+function openNav() {
+    document.getElementById("mobile__menu").style.width = "100%";
 }
 
-function closeNav(){
-document.getElementById("mobile__menu").style.width="0";
+function closeNav() {
+    document.getElementById("mobile__menu").style.width = "0";
 }
- 
